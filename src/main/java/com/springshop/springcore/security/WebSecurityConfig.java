8142e98c6eb80a6b85fa5e9e764d519adf1f3b9e
ktssts <ub.songwenxu@gmail.com>
2021-11-22 23:27:00 +0900

    2-6

--- src/main/java/com/springshop/springcore/security/WebSecurityConfig.java
+++ src/main/java/com/springshop/springcore/security/WebSecurityConfig.java
@@ -12,12 +12,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
+// image 폴더를 login 없이 허용
+                .antMatchers("/images/**").permitAll()
+// css 폴더를 login 없이 허용
+                .antMatchers("/css/**").permitAll()
 // 어떤 요청이든 '인증'
                 .anyRequest().authenticated()
                 .and()
 // 로그인 기능 허용
                 .formLogin()
+                .loginPage("/user/login")
                 .defaultSuccessUrl("/")
+                .failureUrl("/user/login?error")
                 .permitAll()
                 .and()
 // 로그아웃 기능 허용
